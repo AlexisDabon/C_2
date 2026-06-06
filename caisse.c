b079d2027a3585e7e0d99fcb4818588d8cd70fc0
AlexisDabon <xela619375@gmail.com>
2026-06-06 07:47:32 +0200

    do/while

--- caisse.c
+++ caisse.c
@@ -5,4 +5,19 @@ int main () {
 float prix;
 float total = 0;
 
+do { 
+	printf ("entrez le prix de l'article :");
+	scanf ("%f", &prix);
+
+if (prix < 0) {
+	printf ("prix invalide");
+}
+
+else {
+	total += prix;
+}
+
+} while (prix =! 0); 
+
+
 }
