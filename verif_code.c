7d7ad1b9ad96e760723e9b15b222612879a3e818
AlexisDabon <xela619375@gmail.com>
2026-06-09 16:59:37 +0200

    if/else

--- verif_code.c
+++ verif_code.c
@@ -9,5 +9,13 @@ while (erreur < 3) {
 	
 	printf("entrez votre code : \n");
 	scanf("%d", &code);
-
+	
+	if (code == 1234) {
+		break;
+	}
+	
+	else {
+		erreur++;
+		printf("erreur %d / 3 \n", erreur);
+	}
 }
