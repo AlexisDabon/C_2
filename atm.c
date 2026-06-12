41c8e2aee226082a3e8d7b52c2c1701a72358535
AlexisDabon <xela619375@gmail.com>
2026-06-12 17:56:26 +0200

    while if/else

--- atm.c
+++ atm.c
@@ -2,5 +2,21 @@
 #define CODE_CORRECT 1234
 
 int main () {
-	int choix, montant, solde = 500;
-	int code, erreurs = 0;
+int choix, montant, solde = 500;
+int code, erreurs = 0;
+
+	while (erreurs < 3) {
+		printf("entrez votre code : \n");
+		scanf("%d", &code);
+		
+	if (code == CODE_CORRECT) {
+		printf("Accès autorisé ! \n");
+		break;
+	}
+	else {
+		erreurs++;
+		printf("erreurs !");
+	}
+	}
+
+		
