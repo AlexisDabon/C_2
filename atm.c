e8228d365e7445fce19443af789dde82bc9bd21f
AlexisDabon <xela619375@gmail.com>
2026-06-15 16:39:05 +0200

    switch

--- atm.c
+++ atm.c
@@ -27,6 +27,39 @@ int code, erreurs = 0;
 	do { 
 		printf("1.solde 2.retrait 3.Dépôt 4.quitter \n choix:");
 		scanf("%d", &choix);
-	}
+		
+		switch (choix) {
+			case 1 :
+				printf("solde : %d \n", solde);
+				break;
+			case 2 :
+				printf("montant à retirer : \n");
+				scanf("%d", &montant);
+				if (montant <= solde) {
+					solde -= montant;
+					printf("retrait de : %d . nouveau solde : %d \n", montant, solde);
+				}
+				else {
+					printf("solde insuffisant \n");
+				}
+				break;
+			case 3 :
+				printf("montant à déposer : \n");
+				scanf("%d", &montant);
+				solde += montant;
+				printf("dépôt de : %d . nouveau solde : %d \n", montant, solde);
+				break;
+			case 4 : 
+				printf("au revoir \n");
+				break;
+			default : printf("invalide \n");
+				break;
+				}
+	} while (choix !=4);
 
+	return 0;
+}
+
+
+	
 		
