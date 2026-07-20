2eb12e4b98daed60a442facdc707f390a2b2ed74
AlexisDabon <xela619375@gmail.com>
2026-07-20 07:59:01 +0200

    feat: buffer clearing

--- caisse.c
+++ caisse.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 #define EXIT_VALUE 0
 
+void vider_tampon(void) {
+	int c;
+	while ((c = getchar()) != '\n' && c != EOF);
+}
+
+
 int main(void) {
 	
 	float prix;
 	float total = 0;
 
 	do { 
-		printf("Entrez le prix de l'article (0 pour terminer): \n");
-	if (scanf("%f", &prix) != 1) {
-		break;
+		printf("Entrez le prix de l'article (0 pour terminer le programme): \n");
+		if (scanf("%f", &prix) != 1) {
+		printf("Erreur veuillez entrer un nombre valide \n");
+		vider_tampon();
+		continue;
 		}
-	if (prix > 0) {
+	
+	if (prix < 0) {
+		printf("Erreur le prix ne peut pas être négatif \n");
+		} else if (prix > 0) {
 		total += prix;
 		}
-	} while (prix != EXIT_VALUE); 
+	} while (prix != EXIT_VALUE);
 	
-	printf("Le total est de: %.2f \n", total);
+	printf("Le prix total est de: %.2f \n", total);
 	return 0;
 }
-
-
