49a1e129b5b0a46a5d4263635b790b213916ee67
AlexisDabon <xela619375@gmail.com>
2026-07-20 07:13:30 +0200

    feat: initial cash register logic

--- caisse.c
+++ caisse.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
+#define EXIT_VALUE 0
 
-int main () {
+int main(void) {
+	
+	float prix;
+	float total = 0;
 
-float prix;
-float total = 0;
-
-do { 
-	printf ("entrez le prix de l'article : \n");
-	scanf ("%f", &prix);
-
-if (prix < 0) {
-	printf ("prix invalide \n");
+	do { 
+		printf("Entrez le prix de l'article (0 pour terminer): \n");
+	if (scanf("%f", &prix) != 1) {
+		break;
+		}
+	if (prix > 0) {
+		total += prix;
+		}
+	} while (prix != EXIT_VALUE); 
+	
+	printf("Le total est de: %.2f \n", total);
+	return 0;
 }
 
-else {
-	total += prix;
-}
 
-} while (prix != 0); 
-
-printf ("montant total à payer : %.2f \n", total);
-
-return 0;
-
-}
