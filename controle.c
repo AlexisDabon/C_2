175c5a7c1069813bf0d46dd0e3a359a68ca332b5
AlexisDabon <xela619375@gmail.com>
2026-07-25 14:07:40 +0200

    feat: main

--- controle.c
+++ controle.c
@@ -1,5 +1,9 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
+#define CODE_SECRET 1234
+#define MAX_ESSAIS 3
 
 void vider_buffer(void) {
 	int c;
@@ -17,7 +21,7 @@ int code_securise(void) {
 
 	if (retour == 1) {
 		return code;
-	} else { 
+	} else {
 		return - 1;
 	}
 }
@@ -30,4 +34,27 @@ bool verif_code(int code_saisis, int code_secret) {
 	}
 }
 
+int main(void) {
+	int nb_erreurs = 0;
+	bool acces = false;
 	
+	printf("\n === CONTROLE ACCES === \n");
+
+	while (nb_erreurs < MAX_ESSAIS && !acces) {
+		int code_saisis = code_securise();
+		if (verif_code (code_saisis, CODE_SECRET)) {
+			acces = true;
+		} else {
+			nb_erreurs++;
+			printf("Veuillez saisir le code correct \n");
+		}
+	}
+
+	if (acces) {
+		printf("ACCES AUTORISE \n");
+		return EXIT_SUCCESS;
+	} else {
+		printf("ERREUR \n");
+		return EXIT_FAILURE;
+	}
+}
