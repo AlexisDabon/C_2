30ab8d8ee553ad50d9d6f486a9d81f2998bdb3cb
AlexisDabon <xela619375@gmail.com>
2026-07-27 04:01:51 +0200

    fix: indentation

--- retrait.c
+++ retrait.c
@@ -8,39 +8,33 @@ int main () {
 	int erreur = 0;
 	int montant; 
 
-while (erreur < 3) {
-	printf("entrez votre code : \n");
-	scanf("%d", &code);
+	while (erreur < 3) {
+		printf("entrez votre code : \n");
+		scanf("%d", &code);
 
-if (code == CODE_CORRECT) {
+	if (code == CODE_CORRECT) {
 	break;
+	} else { 
+		erreur++;
+		printf("erreur : %d / 3 \n", erreur);
+		}
 	}
 
-else { 
-	erreur++;
-	printf("erreur : %d / 3 \n", erreur);
-	}
-}
-
-if (erreur == 3) {
-	printf("compte bloqué ! \n");
-	return 1;
-	}
-
-else {
-	printf("solde actuel : %d \n", SOLDE_INITIAL);
-	printf("combien voulez vous retirer ? \n");
-	scanf("%d", &montant);
+	if (erreur == 3) {
+		printf("compte bloqué ! \n");
+		return 1;
+	} else {
+		printf("solde actuel : %d \n", SOLDE_INITIAL);
+		printf("combien voulez vous retirer ? \n");
+		scanf("%d", &montant);
 	
-if (montant <= SOLDE_INITIAL) {
-	printf("retrait accordé ! \n");
-	printf("solde actuel %d \n", SOLDE_INITIAL - montant);
+	if (montant <= SOLDE_INITIAL) {
+		printf("retrait accordé ! \n");
+		printf("solde actuel %d \n", SOLDE_INITIAL - montant);
+	} else {
+		printf("solde insuffisant \n");
+		}
 	}
-else {
-	printf("solde insuffisant \n");
-	}
-}
 
-return 0;
-
+	return 0;
 }
